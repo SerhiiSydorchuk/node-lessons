79b584a4d4c7174d0f587bc96638fce4673c0b90
SerhiiSydorchuk <sydorchuks93@gmail.com>
2025-01-04 20:33:05 +0100

    lesson 2

--- main.js
+++ main.js
@@ -1,54 +1,42 @@
 const http = require('node:http');
-// const readline = require("readline")
-
-// const Helper = require('./helpers/helpers.js');
-// console.log('main JS');
-// console.log(__dirname);
-// console.log(__filename);
-// Helper.foo()
-
-
-// Використання readline
-
-// const foo = async () => {
-//     const rlInter = readline.createInterface({
-//         input: process.stdin,
-//         output: process.stdout,
-//             })
-//     rlInter.question('What is your name?', (name)=>{
-//         console.log(`Hello ${name}`);
-//         rlInter.close();
-//     })
-//
-// }
-//
-// void foo ()
+const path = require('node:path')
 
 // Create a local server to receive data from
 const foo = async ()=>{
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+    //http
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'application/json' });
+//
+//     const url = req.url;
+//     if (url=== "/about"){
+//         res.end(JSON.stringify({
+//             data: "About Page"
+//         }))
+//     }
+//     if (url=== "/main"){
+//         res.end(JSON.stringify({
+//             data: "Main Page"
+//         }))
+//     }
+//     if(url === '/'){
+//         res.end(JSON.stringify({
+//             data: 'Hello World!',
+//         }));
+//     }
+//
+// });
+//
+// server.listen(8001);
+    const pathToHelpers = path.join(__dirname,'helpers', 'helpers.js')
+    console.log(pathToHelpers);
+    console.log(path.basename(pathToHelpers));
+    console.log(path.dirname(pathToHelpers));
+    console.log(path.parse(pathToHelpers));
+    console.log(path.normalize('C:\\\\Users\\\\sydor\\\\OneDrive\\\\Desktop\\\\Okten\\\\node-lessons\\\\helpers'));
+    console.log(path.isAbsolute('./helpers/helpers.js'));
 
-    const url = req.url;
-    if (url=== "/about"){
-        res.end(JSON.stringify({
-            data: "About Page"
-        }))
-    }
-    if (url=== "/main"){
-        res.end(JSON.stringify({
-            data: "Main Page"
-        }))
-    }
-    if(url === '/'){
-        res.end(JSON.stringify({
-            data: 'Hello World!',
-        }));
-    }
+}
 
-});
-
-server.listen(8001);}
 void foo()
 
 
